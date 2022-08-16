ddd6c54a9fa04075550c7317ce4937773a0a3a10
husee-code <87664267+husee-code@users.noreply.github.com>
2022-08-16 11:57:03 +0300

    Update vars.py

--- cons_and_vars/vars.py
+++ cons_and_vars/vars.py
@@ -6,13 +6,13 @@ gandoniy_chat = -651037154
 zaebis_chat = -669655344
 
 gandoniy: dict[str:set] = {}
-with open(f'black_list/permanent.json') as js:
+with open(f'root/botProject/mihanProject/black_list/permanent.json') as js:
     black_list: set = set(json.load(js))
 
-for chat in os.listdir('black_list/restricted_chats'):
-    with open(f'black_list/restricted_chats/{chat}') as js:
+for chat in os.listdir('root/botProject/mihanProject/black_list/restricted_chats'):
+    with open(f'root/botProject/mihanProject/black_list/restricted_chats/{chat}') as js:
         chat_name = chat.replace('.json', '')
         gandoniy[chat_name] = set(json.load(js))
 
-with open(r"cons_and_vars\texts.json", encoding='utf-8') as js:
+with open(r"root/botProject/mihanProject/cons_and_vars\texts.json", encoding='utf-8') as js:
     texts = json.load(js)
